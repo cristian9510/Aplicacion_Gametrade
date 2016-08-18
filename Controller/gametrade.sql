559d9ba2918aa0a2dbf0bac25ad077d77a8575ef
cristian9510 <cristian095@hotmail.es>
2016-08-17 20:59:32 -0500

    bd

--- Controller/gametrade.sql
+++ Controller/gametrade.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-06-2016 a las 00:04:05
--- Versión del servidor: 10.1.9-MariaDB
--- Versión de PHP: 5.5.30
+-- Tiempo de generación: 18-08-2016 a las 03:58:13
+-- Versión del servidor: 10.1.10-MariaDB
+-- Versión de PHP: 5.6.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,6 +23,17 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `tbbl_usuario_x_pto`
+--
+
+CREATE TABLE `tbbl_usuario_x_pto` (
+  `pto_cod` int(11) NOT NULL,
+  `usu_cod` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `tbl_auditoria`
 --
 
@@ -32,30 +43,8 @@ CREATE TABLE `tbl_auditoria` (
   `audi_accion` varchar(200) COLLATE utf8_bin NOT NULL,
   `audi_fech` date NOT NULL,
   `audi_estado` int(1) NOT NULL,
-  `audi_desc` text COLLATE utf8_bin NOT NULL
+  `audi_desc` blob NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tbl_bono`
---
-
-CREATE TABLE `tbl_bono` (
-  `bono_cod` int(11) NOT NULL,
-  `cat_bono_cod` int(11) NOT NULL,
-  `pto_cod` int(11) NOT NULL,
-  `bono_desc` int(11) NOT NULL,
-  `bono_cant` int(11) NOT NULL,
-  `bono_estado` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Volcado de datos para la tabla `tbl_bono`
---
-
-INSERT INTO `tbl_bono` (`bono_cod`, `cat_bono_cod`, `pto_cod`, `bono_desc`, `bono_cant`, `bono_estado`) VALUES
-(2147483647, 2, 0, 0, 32, 0);
 
 -- --------------------------------------------------------
 
@@ -66,39 +55,31 @@ INSERT INTO `tbl_bono` (`bono_cod`, `cat_bono_cod`, `pto_cod`, `bono_desc`, `bon
 CREATE TABLE `tbl_categoria_bono` (
   `cat_bono_cod` int(11) NOT NULL,
   `cat_bono_nom` varchar(80) COLLATE utf8_bin NOT NULL,
-  `cat_bono_fech` date NOT NULL,
-  `cat_bono_estado` int(1) NOT NULL
+  `cat_bono_fech` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Volcado de datos para la tabla `tbl_categoria_bono`
---
-
-INSERT INTO `tbl_categoria_bono` (`cat_bono_cod`, `cat_bono_nom`, `cat_bono_fech`, `cat_bono_estado`) VALUES
-(1, 'asdfasff', '2016-06-19', 0),
-(2, 'aaaaa', '2016-06-06', 0),
-(3, 'uuuuu', '2016-06-23', 0);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tbl_categoria_juego`
+-- Estructura de tabla para la tabla `tbl_categoria_jue`
 --
 
-CREATE TABLE `tbl_categoria_juego` (
-  `cat_jue_cod` int(11) NOT NULL,
-  `cat_jue_nom` varchar(80) COLLATE utf8_bin NOT NULL,
-  `cat_jue_desc` text COLLATE utf8_bin NOT NULL,
-  `cat_jue_estado` int(1) NOT NULL,
-  `cat_jue_fech` date NOT NULL
+CREATE TABLE `tbl_categoria_jue` (
+  `cat_cod` int(11) NOT NULL,
+  `cat_nom` varchar(80) COLLATE utf8_bin NOT NULL,
+  `cat_desc` blob NOT NULL,
+  `cat_estado` int(1) NOT NULL,
+  `cat_fech` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Volcado de datos para la tabla `tbl_categoria_juego`
+-- Volcado de datos para la tabla `tbl_categoria_jue`
 --
 
-INSERT INTO `tbl_categoria_juego` (`cat_jue_cod`, `cat_jue_nom`, `cat_jue_desc`, `cat_jue_estado`, `cat_jue_fech`) VALUES
-(12312, 'dasdasd', 'asdasdasdasd', 1, '2016-06-28');
+INSERT INTO `tbl_categoria_jue` (`cat_cod`, `cat_nom`, `cat_desc`, `cat_estado`, `cat_fech`) VALUES
+(200, 'futbol', '', 1, '2016-05-02'),
+(201, 'aventura', '', 1, '2016-05-04'),
+(33, 'ss', 0x73777373, 0, '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -112,6 +93,14 @@ CREATE TABLE `tbl_ciudad` (
   `depar_cod` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- Volcado de datos para la tabla `tbl_ciudad`
+--
+
+INSERT INTO `tbl_ciudad` (`ciu_cod`, `ciu_nom`, `depar_cod`) VALUES
+(1, 'medellin', 1),
+(2, 'cali', 2);
+
 -- --------------------------------------------------------
 
 --
@@ -123,7 +112,7 @@ CREATE TABLE `tbl_comentario` (
   `usu_cod` int(11) NOT NULL,
   `jue_cod` int(11) NOT NULL,
   `coment_asunt` varchar(255) COLLATE utf8_bin NOT NULL,
-  `coment_desc` text COLLATE utf8_bin NOT NULL,
+  `comet_desc` blob NOT NULL,
   `coment_fech` date NOT NULL,
   `coment_estado` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
@@ -147,8 +136,10 @@ CREATE TABLE `tbl_consola` (
 --
 
 INSERT INTO `tbl_consola` (`cons_cod`, `cons_nom`, `cons_estado`, `cons_refer`, `cons_imagen`) VALUES
-(200, 'play station', 1, 2, 'adasdsdsd'),
-(2147483647, 'xbox', 1, 360, '');
+(100, 'play station', 1, 2, ''),
+(101, 'play station', 1, 3, ''),
+(102, 'xbox', 1, 3, ''),
+(201, 'aaaaaa', 0, 0, 'asasa');
 
 -- --------------------------------------------------------
 
@@ -159,7 +150,7 @@ INSERT INTO `tbl_consola` (`cons_cod`, `cons_nom`, `cons_estado`, `cons_refer`, 
 CREATE TABLE `tbl_consola_x_juego` (
   `jue_cod` int(11) NOT NULL,
   `cons_cod` int(11) NOT NULL,
-  `cons_jue_desc` text COLLATE utf8_bin NOT NULL
+  `desc` blob NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -170,9 +161,15 @@ CREATE TABLE `tbl_consola_x_juego` (
 
 CREATE TABLE `tbl_departamento` (
   `depar_cod` int(11) NOT NULL,
-  `depar_nom` varchar(100) COLLATE utf8_bin NOT NULL,
-  `pais_cod` int(11) NOT NULL
+  `depar_nom` varchar(100) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `tbl_departamento`
+--
+
+INSERT INTO `tbl_departamento` (`depar_cod`, `depar_nom`) VALUES
+(1, 'antioquia');
 
 -- --------------------------------------------------------
 
@@ -191,43 +188,13 @@ CREATE TABLE `tbl_encuesta` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tbl_juego`
---
-
-CREATE TABLE `tbl_juego` (
-  `jue_cod` int(11) NOT NULL,
-  `usu_cod` int(11) NOT NULL,
-  `jue_nom` varchar(80) COLLATE utf8_bin NOT NULL,
-  `cons_cod` int(11) NOT NULL,
-  `cat_jue_cod` int(11) NOT NULL,
-  `jue_desc` text COLLATE utf8_bin NOT NULL,
-  `jue_cant` int(11) NOT NULL,
-  `jue_trailer` text COLLATE utf8_bin NOT NULL,
-  `jue_fech_public` date NOT NULL,
-  `jue_imagen` varchar(100) COLLATE utf8_bin NOT NULL,
-  `jue_pal_clave` varchar(50) COLLATE utf8_bin NOT NULL,
-  `jue_estado` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Volcado de datos para la tabla `tbl_juego`
---
-
-INSERT INTO `tbl_juego` (`jue_cod`, `usu_cod`, `jue_nom`, `cons_cod`, `cat_jue_cod`, `jue_desc`, `jue_cant`, `jue_trailer`, `jue_fech_public`, `jue_imagen`, `jue_pal_clave`, `jue_estado`) VALUES
-(200, 2, 'sdgsdfgfdgs', 200, 0, 'sdgdsfg', 34, 'adfas', '2016-06-23', 'afaf', 'asdfas', 1),
-(201, 5, 'sgdsfg', 200, 0, 'sfdgdsf', 23, 'safdsfa', '2016-06-15', 'asdfasf', 'asfasdf', 1),
-(202, 4, 'sgsdfgfds', 200, 0, 'sfgdf', 12, 'asfasdf', '2016-06-29', 'afasdf', 'adsad', 1);
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `tbl_noticia`
 --
 
 CREATE TABLE `tbl_noticia` (
   `noti_cod` int(11) NOT NULL,
   `noti_nom` varchar(80) COLLATE utf8_bin NOT NULL,
-  `noti_desc` text COLLATE utf8_bin NOT NULL,
+  `noti_desc` blob NOT NULL,
   `noti_estado` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
@@ -240,8 +207,15 @@ CREATE TABLE `tbl_noticia` (
 CREATE TABLE `tbl_pack_punto` (
   `pto_cod` int(11) NOT NULL,
   `pto_moneda` int(11) NOT NULL,
-  `pto_desc` text COLLATE utf8_bin NOT NULL
+  `pto_desc` blob NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `tbl_pack_punto`
+--
+
+INSERT INTO `tbl_pack_punto` (`pto_cod`, `pto_moneda`, `pto_desc`) VALUES
+(243, 42423, 0x736461646173);
 
 -- --------------------------------------------------------
 
@@ -277,30 +251,30 @@ CREATE TABLE `tbl_ranking` (
 
 CREATE TABLE `tbl_usuario` (
   `usu_cod` int(11) NOT NULL,
-  `usu_tip_docum` int(1) NOT NULL,
+  `usu_tip_docum` int(11) NOT NULL,
   `usu_num_docum` varchar(20) COLLATE utf8_bin NOT NULL,
-  `usu_photo` varchar(255) COLLATE utf8_bin NOT NULL,
   `usu_pass` varchar(30) COLLATE utf8_bin NOT NULL,
   `usu_nick` varchar(50) COLLATE utf8_bin NOT NULL,
   `usu_nom` varchar(80) COLLATE utf8_bin NOT NULL,
   `usu_apell` varchar(80) COLLATE utf8_bin NOT NULL,
   `rol_cod` int(11) NOT NULL,
   `usu_tel` int(11) NOT NULL,
-  `usu_naci` date NOT NULL,
-  `usu_cel` varchar(20) COLLATE utf8_bin NOT NULL,
+  `usu_naci` int(11) NOT NULL,
+  `usu_cel` int(11) NOT NULL,
   `usu_dir` varchar(100) COLLATE utf8_bin NOT NULL,
   `usu_mail` varchar(100) COLLATE utf8_bin NOT NULL,
   `ciu_cod` int(11) NOT NULL,
   `usu_estado` int(1) NOT NULL,
-  `usu_fech` date NOT NULL
+  `usu_fech` date NOT NULL,
+  `photo` varchar(255) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Volcado de datos para la tabla `tbl_usuario`
 --
 
-INSERT INTO `tbl_usuario` (`usu_cod`, `usu_tip_docum`, `usu_num_docum`, `usu_photo`, `usu_pass`, `usu_nick`, `usu_nom`, `usu_apell`, `rol_cod`, `usu_tel`, `usu_naci`, `usu_cel`, `usu_dir`, `usu_mail`, `ciu_cod`, `usu_estado`, `usu_fech`) VALUES
-(1, 0, '', '', '1234', 'cristian', '', '', 0, 0, '0000-00-00', '', '', '', 0, 0, '0000-00-00');
+INSERT INTO `tbl_usuario` (`usu_cod`, `usu_tip_docum`, `usu_num_docum`, `usu_pass`, `usu_nick`, `usu_nom`, `usu_apell`, `rol_cod`, `usu_tel`, `usu_naci`, `usu_cel`, `usu_dir`, `usu_mail`, `ciu_cod`, `usu_estado`, `usu_fech`, `photo`) VALUES
+(1, 1, '1035231400', '1234', 'cristian', 'cristian camilo', 'echavarria zapata', 1, 4071474, 1233, 320855561, 'barbosa', 'cristian095@hotmail.es', 1, 1, '2016-06-07', 'C:\\xampp1\\htdocs\\GameTrade_aplicacion\\Views\\Imagen\\fotosUsercristian.jpg');
 
 -- --------------------------------------------------------
 
@@ -311,30 +285,61 @@ INSERT INTO `tbl_usuario` (`usu_cod`, `usu_tip_docum`, `usu_num_docum`, `usu_pho
 CREATE TABLE `tbl_usuario_x_encuesta` (
   `usu_cod` int(11) NOT NULL,
   `enc_cod` int(11) NOT NULL,
-  `enc_desc` text COLLATE utf8_bin NOT NULL
+  `enc_des` blob NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tbl_usuario_x_juego`
+-- Estructura de tabla para la tabla `tbl_usuario_x_videojuego`
 --
 
-CREATE TABLE `tbl_usuario_x_juego` (
+CREATE TABLE `tbl_usuario_x_videojuego` (
   `usu_cod` int(11) NOT NULL,
   `jue_cod` int(11) NOT NULL,
-  `usu_jue_desc` text COLLATE utf8_bin NOT NULL
+  `desc` varchar(255) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tbl_usuario_x_pto`
+-- Estructura de tabla para la tabla `tbl_videojuego`
 --
 
-CREATE TABLE `tbl_usuario_x_pto` (
+CREATE TABLE `tbl_videojuego` (
+  `jue_cod` int(11) NOT NULL,
+  `usu_cod` int(11) NOT NULL,
+  `jue_nom` varchar(100) COLLATE utf8_bin NOT NULL,
+  `cons_cod` int(11) NOT NULL,
+  `cat_cod` int(11) NOT NULL,
+  `jue_desc` blob NOT NULL,
+  `jue_cant` int(11) NOT NULL,
+  `jue_trailer` text COLLATE utf8_bin NOT NULL,
+  `jue_fech_public` date NOT NULL,
+  `jue_imagen` varchar(100) COLLATE utf8_bin NOT NULL,
+  `jue_pal_clave` varchar(50) COLLATE utf8_bin NOT NULL,
+  `jue_estado` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `tbl_videojuego`
+--
+
+INSERT INTO `tbl_videojuego` (`jue_cod`, `usu_cod`, `jue_nom`, `cons_cod`, `cat_cod`, `jue_desc`, `jue_cant`, `jue_trailer`, `jue_fech_public`, `jue_imagen`, `jue_pal_clave`, `jue_estado`) VALUES
+(1, 1035231400, 'fifa', 1, 1, 0x6573746f20657320756e20656e7361796f, 3, 'asdasad', '2016-05-05', 'asadsa', 'futbol', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_bono`
+--
+
+CREATE TABLE `tb_bono` (
+  `bono_cod` int(11) NOT NULL,
+  `cat_bono_cod` int(11) NOT NULL,
   `pto_cod` int(11) NOT NULL,
-  `usu_cod` int(11) NOT NULL
+  `bono_desc` blob NOT NULL,
+  `bono_cant` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -342,18 +347,17 @@ CREATE TABLE `tbl_usuario_x_pto` (
 --
 
 --
+-- Indices de la tabla `tbbl_usuario_x_pto`
+--
+ALTER TABLE `tbbl_usuario_x_pto`
+  ADD PRIMARY KEY (`pto_cod`,`usu_cod`);
+
+--
 -- Indices de la tabla `tbl_auditoria`
 --
 ALTER TABLE `tbl_auditoria`
-  ADD PRIMARY KEY (`audi_cod`);
-
---
--- Indices de la tabla `tbl_bono`
---
-ALTER TABLE `tbl_bono`
-  ADD PRIMARY KEY (`bono_cod`),
-  ADD KEY `cat_bono_cod` (`cat_bono_cod`),
-  ADD KEY `pto_cod` (`pto_cod`);
+  ADD PRIMARY KEY (`audi_cod`),
+  ADD KEY `usu_cod` (`usu_cod`);
 
 --
 -- Indices de la tabla `tbl_categoria_bono`
@@ -372,7 +376,9 @@ ALTER TABLE `tbl_ciudad`
 -- Indices de la tabla `tbl_comentario`
 --
 ALTER TABLE `tbl_comentario`
-  ADD PRIMARY KEY (`coment_cod`);
+  ADD PRIMARY KEY (`coment_cod`),
+  ADD KEY `usu_cod` (`usu_cod`),
+  ADD KEY `jue_cod` (`jue_cod`);
 
 --
 -- Indices de la tabla `tbl_consola`
@@ -390,23 +396,13 @@ ALTER TABLE `tbl_consola_x_juego`
 -- Indices de la tabla `tbl_departamento`
 --
 ALTER TABLE `tbl_departamento`
-  ADD PRIMARY KEY (`depar_cod`),
-  ADD KEY `pais_cod` (`pais_cod`);
+  ADD PRIMARY KEY (`depar_cod`);
 
 --
 -- Indices de la tabla `tbl_encuesta`
 --
 ALTER TABLE `tbl_encuesta`
   ADD PRIMARY KEY (`enc_cod`);
-
---
--- Indices de la tabla `tbl_juego`
---
-ALTER TABLE `tbl_juego`
-  ADD PRIMARY KEY (`jue_cod`),
-  ADD KEY `usu_cod` (`usu_cod`),
-  ADD KEY `cons_cod` (`cons_cod`),
-  ADD KEY `cat_jue_cod` (`cat_jue_cod`);
 
 --
 -- Indices de la tabla `tbl_noticia`
@@ -436,27 +432,28 @@ ALTER TABLE `tbl_ranking`
 -- Indices de la tabla `tbl_usuario`
 --
 ALTER TABLE `tbl_usuario`
-  ADD PRIMARY KEY (`usu_cod`),
-  ADD KEY `rol_cod` (`rol_cod`),
-  ADD KEY `usu_tip_docum` (`usu_tip_docum`);
+  ADD PRIMARY KEY (`usu_cod`);
 
 --
 -- Indices de la tabla `tbl_usuario_x_encuesta`
 --
 ALTER TABLE `tbl_usuario_x_encuesta`
-  ADD PRIMARY KEY (`usu_cod`,`enc_cod`);
+  ADD PRIMARY KEY (`enc_cod`,`usu_cod`);
 
 --
--- Indices de la tabla `tbl_usuario_x_juego`
+-- Indices de la tabla `tbl_usuario_x_videojuego`
 --
-ALTER TABLE `tbl_usuario_x_juego`
+ALTER TABLE `tbl_usuario_x_videojuego`
   ADD PRIMARY KEY (`usu_cod`,`jue_cod`);
 
 --
--- Indices de la tabla `tbl_usuario_x_pto`
+-- Indices de la tabla `tbl_videojuego`
 --
-ALTER TABLE `tbl_usuario_x_pto`
-  ADD PRIMARY KEY (`pto_cod`,`usu_cod`);
+ALTER TABLE `tbl_videojuego`
+  ADD PRIMARY KEY (`jue_cod`),
+  ADD UNIQUE KEY `cat_cod` (`cat_cod`),
+  ADD KEY `usu_cod` (`usu_cod`),
+  ADD KEY `cons_cod` (`cons_cod`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -468,15 +465,10 @@ ALTER TABLE `tbl_usuario_x_pto`
 ALTER TABLE `tbl_auditoria`
   MODIFY `audi_cod` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT de la tabla `tbl_bono`
---
-ALTER TABLE `tbl_bono`
-  MODIFY `bono_cod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2147483647;
---
 -- AUTO_INCREMENT de la tabla `tbl_categoria_bono`
 --
 ALTER TABLE `tbl_categoria_bono`
-  MODIFY `cat_bono_cod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `cat_bono_cod` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `tbl_comentario`
 --
--- Views/header_Usu.php
+++ Views/header_Usu.php
@@ -1,22 +1,23 @@
 <header>
 	<div id="e63Perfil">Mi perfil</div>
-
+	<div id="e64ConteDato">
 		<div id="e64Photo"></div>
-		
+
 		<ul id="ulDatos">
 			<li>Nombre</li>
 			<li>Puntos</li>
 			<li><div id="e64Public">Publicar juego</div></li>
 		</ul>
+	</div>
 
 	<div id="FondMe">
-		<div id="e64LogoU"></div>
+		<!--<div id="e64LogoU"></div>
 		<ul id="menuUsu">
 			<li>Inicio</li>
 			<li>Buscar</li>
 			<li>Mis juegos</li>
 			<li>Tendencias</li>
 			<li>Tienda</li>
-		</ul>
+		</ul>-->
 	</div>
 </header>
