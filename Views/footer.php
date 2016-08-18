d8439913cad2090b110f9e36b82663b897b079b7
cristian9510 <cristian095@hotmail.es>
2016-08-18 14:06:23 -0500

    footer

---
+++ Views/footer.php
@@ -0,0 +1,9 @@
+<footer>
+	<h1 style="text-align: center;">Siguenos tambien aqui</h1>
+
+	<p style="text-align: center;">
+	Correo Electronico: gametrade@hotmail.com.</p>
+
+	<p style="text-align: center;">Copyright 2016 ciudadanoo
+	tempor incididunt .</p>
+</footer>
