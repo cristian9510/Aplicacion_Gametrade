e2c1b937c667d8666efcc6fda71e3d7bae53e573
1014793 <acorrea829@misena.edu.co>
2016-08-18 14:32:25 -0500

    para terminar

--- Views/footer.php
+++ Views/footer.php
@@ -1,5 +1,19 @@
 <footer>
+    
 	<h1 style="text-align: center;">Siguenos tambien aqui</h1>
+
+	<div class="social">
+		<ul>
+			<li><a href="http://www.facebook.com/falconmasters" target="_blank" class="icon-facebook"></a></li>
+			<li><a href="http://www.twitter.com/falconmasters" target="_blank" class="icon-twitter"></a></li>
+			<li><a href="http://www.googleplus.com/falconmasters" target="_blank" class="icon-googleplus"></a></li>
+			<li><a href="http://www.pinterest.com/falconmasters" target="_blank" class="icon-pinterest"></a></li>
+			<li><a href="mailto:contacto@falconmasters.com" class="icon-mail"></a></li>
+		</ul>
+	</div>
+
+
+
 
 	<p style="text-align: center;">
 	Correo Electronico: gametrade@hotmail.com.</p>
